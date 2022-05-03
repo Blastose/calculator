12cbe84843fa7749bbe16dbb65a5a7aef8635460
Blastose <blastoisejay@gmail.com>
2022-05-03 13:31:40 -0700

    Use state machine for calculator logic

--- calculator.js
+++ calculator.js
@@ -35,40 +35,150 @@ function operate(operator, arg1, arg2) {
   return result;
 }
 
-let arg1 = 0;
-let arg2 = 0;
-let operatorSign = "";
+function appendDigitToNumber(currentNum, nextDigit) {
+  if (currentNum === "0") {
+    if (nextDigit === "0") {
+      return currentNum;
+    } else {
+      return nextDigit;
+    }    
+  } else if (currentNum.includes(".") && nextDigit === ".") {
+    return currentNum;
+  }
+  return `${currentNum}${nextDigit}`;
+}
+
+function logValues() {
+  console.log("arg1: " + arg1);
+  console.log("arg2: " + arg2);
+  console.log("operator: " + operatorSign);
+}
+
+const stateMachine = {
+  state: 'INPUT_FOR_ARG1',
+  arg1: "0",
+  arg2: "0",
+  operator: "+",
+  transitions: {
+    INPUT_FOR_ARG1: {
+      pressNumber(number) {
+        this.arg1 = appendDigitToNumber(this.arg1, number);
+        this.state = 'INPUT_FOR_ARG1';
+
+        console.log('pressed number from INPUT_FOR_ARG1 state');
+      },
+      pressOperator(operatorSign) {
+        this.operator = operatorSign;
+        this.state = 'INPUT_FOR_OPERATOR';
+
+        console.log('pressed operator from INPUT_FOR_ARG1 state');
+      },
+      pressEquals() {
+        console.log("equals: " + operate(this.operator, Number(this.arg1), Number(this.arg2)));
+        this.state = 'CALCULATE_RESULT';
+
+        console.log('pressed = from INPUT_FOR_ARG1 state');
+      },
+    },
+    INPUT_FOR_OPERATOR: {
+      pressNumber(number) {
+        this.arg2 = appendDigitToNumber(this.arg2, number);
+        this.state = 'INPUT_FOR_ARG2';
+        
+        console.log('pressed number from INPUT_FOR_OPERATOR state');
+      },
+      pressOperator(operatorSign) {
+        this.operator = operatorSign;
+        this.state = 'INPUT_FOR_OPERATOR';
+
+        console.log('pressed operator from INPUT_FOR_OPERATOR state');
+      },
+      pressEquals() {
+        this.state = 'CALCULATE_RESULT';
+        let result = operate(this.operator, Number(this.arg1), Number(this.arg2));
+        this.arg1 = String(result);
+
+        console.log('pressed = from INPUT_FOR_OPERATOR state');
+      },
+    },
+    INPUT_FOR_ARG2: {
+      pressNumber(number) {
+        this.arg2 = appendDigitToNumber(this.arg2, number);
+        this.state = 'INPUT_FOR_ARG2';
+
+        console.log('pressed number from INPUT_FOR_ARG2 state');
+      },
+      pressOperator(operatorSign) {
+        let result = operate(this.operator, Number(this.arg1), Number(this.arg2));
+        this.arg1 = String(result);
+        this.arg2 = "0";
+        this.operator = operatorSign;
+        this.state = 'INPUT_FOR_OPERATOR';
+
+        console.log('pressed operator from INPUT_FOR_ARG2 state');
+      },
+      pressEquals() {
+        this.state = 'CALCULATE_RESULT';
+        let result = operate(this.operator, Number(this.arg1), Number(this.arg2));
+        console.log("equals: " + result);
+        this.arg1 = String(result);
+
+        console.log('pressed = from INPUT_FOR_ARG2 state');
+      },
+    },
+    CALCULATE_RESULT: {
+      pressNumber(number) {
+        this.arg1 = appendDigitToNumber("0", number);
+        this.state = 'INPUT_FOR_ARG1';
+
+        console.log('pressed number from CALCULATE_RESULT state');
+      },
+      pressOperator(operatorSign) {
+        this.operator = operatorSign;
+        this.arg2 = "0";
+        this.state = 'INPUT_FOR_OPERATOR';
+
+        console.log('pressed operator from CALCULATE_RESULT state');
+      },
+      pressEquals() {
+        this.state = 'CALCULATE_RESULT';
+        let result = operate(this.operator, Number(this.arg1), Number(this.arg2));
+        console.log("equals: " + result);
+        this.arg1 = String(result);
+
+        console.log('pressed = from CALCULATE_RESULT state');
+      },
+    },
+  },
+  dispatch(actionName, value) {
+    const action = this.transitions[this.state][actionName];
+    if (action) {
+      action.call(this, value);
+      console.log("state: " + this.state);
+      console.log("arg1: " + this.arg1);
+      console.log("arg2: " + this.arg2);
+      console.log("op: " + this.operator);
+    } else {
+      console.log('Invalid action');
+    }
+  },
+};
+
 const digitButtons = document.querySelectorAll('.calc-key');
 digitButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    const display = document.getElementById('number-calc-curr-display');
-    display.textContent = `${display.textContent}${e.target.value}`;
+    stateMachine.dispatch('pressNumber', e.target.value);
   });
 })
 
 const operators = document.querySelectorAll('.operator');
 operators.forEach((operator) => {
   operator.addEventListener('click', (e) => {
-    const display = document.getElementById('number-calc-curr-display');
-    const above = document.getElementById('number-calc-above-display');
-    arg2 = arg1;
-    arg1 = Number(display.textContent);
-    above.textContent = `${arg1} ${e.target.value}`;
-    display.textContent = "";
-    operatorSign = e.target.value;
+    stateMachine.dispatch('pressOperator', e.target.value);
   });
 });
 
 const equals = document.querySelector('.equals');
 equals.addEventListener('click', () => {
-  const display = document.getElementById('number-calc-curr-display');
-  arg2 = Number(display.textContent);
-  let result = operate(operatorSign, arg1, arg2);
-  console.log("arg1: " + arg1);
-  console.log("arg2: " + arg2);
-  console.log("operator: " + operatorSign);
-  console.log(result);
-  display.textContent = result;
-  const above = document.getElementById('number-calc-above-display');
-  above.textContent = "";
+  stateMachine.dispatch('pressEquals');
 });
